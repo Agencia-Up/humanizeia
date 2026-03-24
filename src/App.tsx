e1b0c9edac0d5e48009325f2d87202c130c34d92
Douglas Aloan <douglasaloan@gmail.com>
2026-03-24 16:25:17 -0300

    fix: restore CRM Contacts route lost in merge

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,7 @@ const WhatsAppCAPI = lazy(() => import("./pages/WhatsAppCAPI"));
 const ApolloDashboard = lazy(() => import("./pages/ApolloDashboard"));
 const SalomaoOrchestrator = lazy(() => import("./pages/SalomaoOrchestrator"));
 const FluxCRM = lazy(() => import("./pages/FluxCRM"));
+const CRMContacts = lazy(() => import("./pages/CRMContacts"));
 const CreativeIntelligence = lazy(() => import("./pages/CreativeIntelligence"));
 const CompetitorRadar = lazy(() => import("./pages/CompetitorRadar"));
 const LeadManagement = lazy(() => import('./pages/LeadManagement'));
@@ -106,6 +107,7 @@ const App = () => (
             <Route path="/apollo" element={<ProtectedRoute><ApolloDashboard /></ProtectedRoute>} />
             <Route path="/salomao" element={<ProtectedRoute><SalomaoOrchestrator /></ProtectedRoute>} />
             <Route path="/crm" element={<ProtectedRoute><FluxCRM /></ProtectedRoute>} />
+            <Route path="/crm/contacts" element={<ProtectedRoute><CRMContacts /></ProtectedRoute>} />
             <Route path="/creative-intelligence" element={<ProtectedRoute><CreativeIntelligence /></ProtectedRoute>} />
             <Route path="/competitor-radar" element={<ProtectedRoute><CompetitorRadar /></ProtectedRoute>} />
             <Route path="/leads" element={<ProtectedRoute><LeadManagement /></ProtectedRoute>} />
